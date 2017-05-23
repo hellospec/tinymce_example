ce813bd4fb47342ad0bde36944b307c53f3efe9c
worrawutp <worrawutp@gmail.com>
2017-05-24 06:16:15 +0700

    add Image and ImageUploader for handler upload image

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'tinymce-rails'
+gem 'mini_magick'
+gem 'carrierwave', '~> 1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522233534) do
+ActiveRecord::Schema.define(version: 20170523143637) do
+
+  create_table "images", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
